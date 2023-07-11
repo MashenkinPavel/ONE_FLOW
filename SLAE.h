a45358c9e1d0c904f46ecf4b074e5a4da292ede8
PavelM <mashenkin.pavel@gmail.com>
2023-07-12 00:29:30 +0300

    Add to project PETSc

--- SLAE.h
+++ SLAE.h
@@ -1,25 +1,18 @@
+#ifndef __SLAE_H
+#define __SLAE_H
+
 //
 // Created by pavel on 03.05.2023.
 //
-#pragma once
+static char help[] = "Solves a tridiagonal linear system with KSP.\n\n";
 
-#include <vector>
+#include <ostream>
+#include <memory>
+#include <petscksp.h>
 
-template <class MType>
-class SLAE {
-public:
-    SLAE(){
-        elems.resize(0);
-        col_ind.resize(0);
-        shift.resize(0);
-    }
-    ~SLAE(){}
-    void Add (int i, int j, MType val){
 
-    }
-private:
-    std::vector<int> col_ind;
-    std::vector<int> shift;
-    std::vector<MType> elems;
-};
+int testfunc1(int argc, char*argv[]);
 
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "mesh.h"
+#include "SLAE.h"
 
 int main(int argc, char* argv[]){
     std::cout<<"Program for calculating CFD\n";
     std::cout<<"made by Mashenkin Pavel\n";
+
+    {
+        Mesh mesh;
+        mesh.Read_msh_file("../test_data/mesh.msh");
+        mesh.Write_vtk_file("../test_data/mesh.vtk");
+    }
 
     {
         Mesh mesh;
@@ -23,6 +30,10 @@ int main(int argc, char* argv[]){
         mesh.Write_vtk_file("./test_data/mesh_2tetr.vtk");
     }
 
+
+    {
+        testfunc1(argc,argv);
+    }
 
 
 
